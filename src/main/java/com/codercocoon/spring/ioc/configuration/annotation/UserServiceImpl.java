0046991385cbdc18ba8c4121f10167d999ca634c
Codercocoon <mounir.sahrani@gmail.com>
2018-11-21 17:26:39 +0100

    This example presents the basic concept of using in Spring the Annotation based configuration.
    This example presents the basic concept of using in Spring the Annotation based configuration.

---
+++ src/main/java/com/codercocoon/spring/ioc/configuration/annotation/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.codercocoon.spring.ioc.configuration.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author contact@codercocoon.com
+ *
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+	private UserDao userDao;
+
+	@Autowired
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public void lockAccountUser(int userId) {
+		userDao.lockAccount(userId, true);
+	}
+
+	@Override
+	public void unlockAccountUser(int userId) {
+		userDao.lockAccount(userId, false);
+	}
+
+	@Override
+	public String getUserinformation(int userId) {
+		User user = userDao.find(userId);
+		return user.getFisrtName() + " - " + user.getLastName() + " - " + user.isAccountlocked();
+	}
+}
